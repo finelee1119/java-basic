25eba32666d84e516b7450c6d369ce21f28891d6
finelee1119 <finelee1119@gmail.com>
2024-02-27 21:57:59 +0900

    java-basic

---
+++ src/memory/Data.java
@@ -0,0 +1,16 @@
+package memory;
+
+public class Data {
+    // 멤버 변수
+    private int value;
+
+    // 생성자
+    public Data(int value) {
+        this.value = value;
+    }
+
+    // 메서드
+    public int getValue() {
+        return value;
+    }
+}
